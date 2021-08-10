ae5c2fd4b15687e2ce8ffb502f650e4f58d217e7
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-08-10 13:06:56 +0530

    initialized reviews app

--- App.js
+++ App.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import {StyleSheet, View , TouchableWithoutFeedback , Keyboard } from 'react-native';
-import Header from './Components/Header';
-import Lister from './Components/Lister';
+import {StyleSheet, View } from 'react-native';
 
 
 const App = () => {
 
   return (
-    <TouchableWithoutFeedback 
-    onPress= {() => Keyboard.dismiss()}
-    >
-    <View style={styles.container} >
-        <Header />
-        <View style={styles.content} >
-          <Lister />
-        </View>     
+    <View>
+
     </View>
-    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    flex:1
-  },
-  content :{
-    padding : 40,
-    flex : 1,
-    marginBottom : 1
-  }
+
 });
 
 export default App;
