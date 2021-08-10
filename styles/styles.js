2fb857ed251a1ecd6ddaddbdea24752ddc4cf2fe
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-08-10 15:23:58 +0530

    managed styles and packages

---
+++ styles/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container :{
+    padding : 24,
+    flex : 1
+},
+  title :{
+    fontFamily : 'regular',
+    fontSize : 18,
+    color : '#333'
+  },
+  paragraph : {
+      marginVertical : 8,
+      lineHeight : 20
+  }
+});
+
+export default styles;
